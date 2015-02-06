0fd179ac7d4066b3d91d96a88d4b72f9ebe8987e
Yundi Huang <kenkaka.hyd@gmail.com>
2015-02-05 22:52:58 -0500

    added general template

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,20 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  product = Product.new
+  assert product.invalid?
+  assert product.errors[:title].any?
+  assert product.errors[:description].any?
+  assert product.errors[:price].any?
+  assert product.errors[:image_url].any?
+
+  test "Product price must be positive" do
+  	product = Product.new(title: "my book",
+  						   description: "yyy",
+  						   image_url: "zzz.jpg")
+  	product.price = -1
+  	assert product.invalid?
+  	product.price = 1
+  	assert product.invalid?
+  end
 end
